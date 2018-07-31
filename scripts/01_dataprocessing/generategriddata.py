093ce32f5417940aa92e52e16de6fb2721857593
mrlmautner <37315823+mrlmautner@users.noreply.github.com>
2018-07-31 16:52:21 -0700

    input data
    set-up for new model data

--- scripts/01_dataprocessing/generategriddata.py
+++ scripts/01_dataprocessing/generategriddata.py
@@ -4,23 +4,9 @@ Created on Thu Jul 12 16:38:07 2018
 
 @author: MM
 """
-import os
+
 import numpy as np
 from osgeo import gdal
-
-os.chdir(r'C:\Users\MM\Google Drive\UrbanGW')
-#os.chdir(r'C:\Users\MM\Google Drive\Tlalpan\Simulation_Wrapper')
-
-## Initialize model extent
-# Lower left corner: (455000, 2107000)
-# Upper right corner: (539000, 2175000)
-xll = 455000
-yll = 2107000
-xur = 539000
-yur = 2175000
-cellsize = 500
-ncols = int((xur-xll)/cellsize) # ncols
-nrows = int((yur-yll)/cellsize) # nrows
 
 # Generate .asc header
 def getHeader(ncols,nrows,xll,yll,cellsize,noData):
@@ -32,7 +18,7 @@ def getHeader(ncols,nrows,xll,yll,cellsize,noData):
     header += "NODATA_value     %s" % noData
     return header
 
-def sample_Model_Griddata(filename,newfile,header):
+def sampleGD(filename,newfile,header,xll,yll,xur,yur,cellsize):
     ds = gdal.Warp(newfile,filename,outputBounds=[xll, yll, xur, yur],xRes=cellsize,yRes=cellsize,resampleAlg=gdal.GRA_Mode)
     band = ds.GetRasterBand(1)
     dsAsArray = band.ReadAsArray()
@@ -40,7 +26,7 @@ def sample_Model_Griddata(filename,newfile,header):
     np.savetxt(newfile, dsAsArray, header=header, fmt="%d",comments='')
     return dsAsArray
 
-def average_Model_Griddata(filename,newfile,header):
+def averageGD(filename,newfile,header,xll,yll,xur,yur,cellsize):
     ds = gdal.Warp(newfile,filename,outputBounds=[xll, yll, xur, yur],xRes=cellsize,yRes=cellsize,resampleAlg=gdal.GRA_Average)
     band = ds.GetRasterBand(1)
     dsAsArray = band.ReadAsArray()
@@ -49,48 +35,3 @@ def average_Model_Griddata(filename,newfile,header):
     np.savetxt(newfile, dsAsArray, header=header, fmt="%1.2f",comments='')
     return dsAsArray
 
-#%%
-# Precipitation rasters
-for year in range(1984,2014):
-    for month in range(1,13):
-        filename = r'data\RawFiles\Precipitation\Precip_VM\Precip_mm_' + str(year) + '_' + '{num:02d}'.format(num=month) + '.asc'
-        newfile = r'data\Input\RCH\Precip_' + str(year) + '_' + '{num:02d}'.format(num=month) + '.asc'
-        header = getHeader(ncols,nrows,xll,yll,cellsize,-99999)
-        dsAsArray = average_Model_Griddata(filename,newfile,header)
-
-# Land Use rasters
-for year in [1984,1997,2003,2010,2012,2016]:
-    filename = r'data\RawFiles\LandUse\LU_' + str(year) + '_ZONE.asc'
-    newfile = r'data\Input\LU_' + str(year) + '.asc'
-    header = getHeader(ncols,nrows,xll,yll,cellsize,-99999)
-    dsAsArray = sample_Model_Griddata(filename,newfile,header)
-    
-# Geology raster
-filename = r'data\RawFiles\Geology\GEOLOGY_ZONES_M.asc'
-newfile = r'data\Input\GEO_VM.asc'
-header = getHeader(ncols,nrows,xll,yll,cellsize,-99999)
-dsAsArray = sample_Model_Griddata(filename,newfile,header)
-
-# DEM raster
-filename = r'data\RawFiles\TopoRasters\DEM_Clipped.asc'
-newfile = r'data\Input\DEM_VM.asc'
-header = getHeader(ncols,nrows,xll,yll,cellsize,-99999)
-dsAsArray = average_Model_Griddata(filename,newfile,header)
-
-#%%
-
-# Model 
-filename = r'data\RawFiles\MODEL_ACTIVE.asc'
-newfile = r'data\Input\ACTIVE.asc'
-header = getHeader(ncols,nrows,xll,yll,cellsize,-99999)
-dsAsArray = sample_Model_Griddata(filename,newfile,header)
-        
-## Well data
-#wellArray = np.loadtxt(r'C:\Users\MM\Google Drive\Tlalpan\Simulation_Wrapper\data\RawFiles\PumpingWells\PUMPING_M3_D_MC.csv', delimiter=',', skiprows=1, usecols=[3,4,7]) # (m3/day)
-#PUMP_annual = np.zeros((30,3))
-#PUMP_annual[:,0] = np.arange(1984,2014)
-#for w in range(3003):
-#    for i in range(0,int(wellArray[w,1] - wellArray[w,0])):
-#        PUMP_annual[int(wellArray[w,0]+i-1984),1] += 1
-#        PUMP_annual[int(wellArray[w,0]+i-1984),2] += wellArray[w,2]
-#np.savetxt(r'C:\Users\MM\Google Drive\Tlalpan\Simulation_Wrapper\data\ModelInput\PUMP_annual.csv', PUMP_annual, delimiter=',')
---
+++ scripts/01_dataprocessing/pumpingvspopulation.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 17 19:37:14 2018
+
+@author: Marina Mautner
+"""
+import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+df = pd.read_csv('C:/Users/MM/Google Drive/ValleMexico/Data/Analysis/Pumping/PUMP_POP_INEGI_REPDA_20180320.csv')
+df = df.set_index(pd.to_datetime(df.Year.apply(lambda x: str(x)+'/01/01')))
+
+PSeries = df.PopZMVM_Census.dropna()
+#PSeries = PSeries.append(df.PopZMVM_Proj.dropna())
+t = df.Year[PSeries.index].values - 1895
+P = PSeries.values
+
+t1 = np.array([1960,1970,1980,1990,1995,2000,2005,2010])
+p1 = df.PopZMVM_Census.dropna()['1960/01/01':]
+t2 = np.array([2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,
+               2023,2024,2025,2026,2027,2028,2029,2030])
+p2 = df.PopZMVM_Proj.dropna()
+
+# Fit population curve
+g1 = np.polyfit(t1, p1, 2)
+f1 = np.poly1d(g1)
+
+g2 = np.polyfit(t2, p2, 2)
+f2 = np.poly1d(g2)
+
+x = np.arange(1960,2030,1)
+Pt1 = np.zeros(len(x))
+Pt2 = np.zeros(len(x))
+for i in range(0,len(x)):
+    Pt1[i] = f1(x[i])
+    Pt2[i] = f2(x[i])
+    
+plt.plot(np.array(df.Year),np.array(df.PopZMVM_Proj),'.',color='black')
+plt.plot(t1,p1,'s',color='grey',zorder=3)
+plt.plot(x,Pt1,'--',color='red')
+plt.plot(x,Pt2,'--',color='blue')
+plt.grid(True)
+plt.xlabel('Year')
+plt.ylabel('Population')
+plt.legend(['CONAPO 2014 Projections','1895 to 2010 Census Data', 'Census Model','CONAPO Projection Model'])
+
+#%%
+PM = pd.DataFrame(np.transpose(np.array([x,Pt2])),columns=['Year','P'])
+PM = PM.set_index(pd.to_datetime(PM.Year.apply(lambda x: str(int(x))+'/01/01')))
+PM.to_csv('C:/Users/MM/Google Drive/ValleMexico/Data/Analysis/Pumping/POP_CM.csv')
+
+Pop = PM.P['2005-01-01':'2016-01-01']
+Pop['2011-01-01'] = np.nan
+Pop = Pop.dropna().values
+Q = df.REPDA_m3d.dropna().values
+
+WEL = pd.DataFrame(np.transpose(np.array([Q,Pop])/10**6),index=df.REPDA_m3d.dropna().index,columns=['Q','P'])
+
+g = np.polyfit(Pop, Q, 1)
+f = np.poly1d(g)
+
+x = np.arange(19*10**6,23*10**6,10**5)
+Qx = np.zeros(len(x))
+for i in range(0,len(x)):
+    Qx[i] = g[0]*x[i] + g[1]
+    
+residuals = f(Pop) - Q
+ss_res = np.sum(residuals**2)
+ss_tot = np.sum((Q-np.mean(Q))**2)
+r_squared = 1 - (ss_res / ss_tot)
+rmse = np.sqrt(ss_res/len(Pop))
+
+plt.grid(True)
+sns.regplot(x='P', y='Q', data=WEL,marker='.',order=1)
+plt.xlabel('Population (millions)')
+plt.ylabel('Groundwater Concessions (' r'$hm^3/d$' ')')
+print(f)
