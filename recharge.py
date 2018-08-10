e54a3235a8f64a79795584877af65bb0d370e3cf
mrlmautner <37315823+mrlmautner@users.noreply.github.com>
2018-08-09 17:33:03 -0700

    Remove land use multiplier
    Reduce function to applying a multiplier for geology related infiltration, monthly summed precip, and average precip

--- recharge.py
+++ recharge.py
@@ -19,35 +19,15 @@ rchFolder = 'UrbanGW//data_output//recharge//'
  
 #%%
 DAYS_M = [31,28,31,30,31,30,31,31,30,31,30,31]
-LU = {}
-for year in [1985,1990,1995,2000,2005,2010,2015]:
-    filename = r'UrbanGW\data_output\LU_' + str(year) + '.asc'
-    LU_Array = gen.openASC(filename)
-    LU[str(year)] = LU_Array
     
-GEO = gen.openASC(r'UrbanGW\data_output\GEO_VM.asc')
+GEO = gen.openASC('UrbanGW//data_output//GEO_VM.asc',1)
 
 #%% 1% Multiplier for lacustrine clays and 100% for all others
 geoMult = 0.01*(GEO==1)+(GEO!=1)
 
 #%%
 header = gen.getHeader(ncols,nrows,xll,yll,cellsize,-99999)
-for year in range(1984,2014):
-    if year < 1988:
-        landusetxt = '1985'
-    elif 1988 <= year < 1993:
-        landusetxt = '1990'
-    elif 1993 <= year < 1998:
-        landusetxt = '1995'
-    elif 1998 <= year < 2003:
-        landusetxt = '2000'
-    elif 2003 <= year < 2008:
-        landusetxt = '2005'
-    elif 2008 <= year < 2013:
-        landusetxt = '2010'
-    elif 2013 <= year < 2014:
-        landusetxt = '2015'
-    
+for year in range(1984,2014):    
     for month in range(1,13):
         
         filename = rchFolder + 'Precip_' + str(year) + '_' + '{num:02d}'.format(num=month) + '.asc'
@@ -56,7 +36,7 @@ for year in range(1984,2014):
         # Apply 1% recharge rate on the clay layer and separate by Recharge potential by LU type
         recharge = geoMult*precip/1000/DAYS_M[month-1]
         
-        newfile = rchFolder + 'ClayMult\\RCH_' + str(year) + '_' + '{num:02d}'.format(num=month) + '.asc'
+        newfile = rchFolder + 'claymult\\RCH_' + str(year) + '_' + '{num:02d}'.format(num=month) + '.asc'
         np.savetxt(newfile, recharge, header=header, fmt="%1.5f",comments='')
 
 #%% Create Average Precipitation RCH file for first time step
@@ -75,7 +55,7 @@ avgPrecip = avgPrecip/nprecip
 
 recharge = geoMult*avgPrecip/1000/30
 
-newfile = rchFolder + 'ClayMult\\RCH_AVG.asc'
+newfile = rchFolder + 'claymult\\RCH_AVG.asc'
 
 np.savetxt(newfile, recharge, header=header, fmt="%1.5f",comments='')
 
@@ -95,5 +75,5 @@ for year in range(1984,2014):
 
 for month in range(0,12):
     avgPrecip[month] = avgPrecip[month]/nprecip
-    newfile1 = rchFolder + 'MTHLY\\AVG_' + '{num:02d}'.format(num=month) + '.asc'
+    newfile1 = rchFolder + 'mthly\\AVG_' + '{num:02d}'.format(num=month) + '.asc'
     np.savetxt(newfile1, avgPrecip[month], header=header, fmt="%1.5f",comments='')
