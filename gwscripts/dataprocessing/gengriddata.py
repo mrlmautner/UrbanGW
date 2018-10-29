952b3c4dc44e673afc55380b157fbc45db8b0f8d
mrlmautner <37315823+mrlmautner@users.noreply.github.com>
2018-10-28 22:11:06 -0700

    Update gwscripts/dataprocessing/gengriddata.py
    Format new monthly pumping data from SACM into csv format for Well Dictionary function

--- gwscripts/dataprocessing/gengriddata.py
+++ gwscripts/dataprocessing/gengriddata.py
@@ -7,6 +7,7 @@ Created on Thu Jul 12 16:38:07 2018
 
 import numpy as np
 from osgeo import gdal
+import pandas as pd
 
 # Generate .asc header
 def getHeader(ncols,nrows,xll,yll,cellsize,noData):
@@ -40,3 +41,38 @@ def averageGD(filename,newfile,header,xll,yll,xur,yur,cellsize,band=1):
     
     np.savetxt(newfile, dsAsArray, header=header, fmt="%1.2f",comments='')
     return dsAsArray
+
+def generateWellData(filename, pandasWD, year):
+    mths = ["ENE","FEB","MAR","ABR","MAY","JUN","JUL","AGO","SEP","OCT","NOV","DIC"]
+    days_m = [31,28,31,30,31,30,31,31,30,31,30,31]
+    wellFeatures = ['X','Y','CVE_ENT','CVE_MUN','NOM_MUN','CODE']
+    
+    newWDset = pd.read_csv(filename,skiprows=6,usecols=range(2,21),index_col=2,na_values='#VALUE!')
+    newWDset[mths] = newWDset[mths].apply(pd.to_numeric, errors='coerce')
+    newWDset = newWDset.drop(labels='nan')
+    
+    temp = wellFeatures.copy()
+    temp.append('Flow_m3')
+    stackedWDset = pd.DataFrame(columns=temp)
+    
+    for i,m in enumerate(mths):
+        temp = wellFeatures.copy()
+        temp.append(m)
+        tempPD = newWDset[temp].rename(columns={m:'Flow_m3'})
+        tempPD.Flow_m3 *= -1/days_m[i]
+        tempPD['S_PER'] = pd.Series(np.ones(newWDset.shape[0])*(year-1984)*12+1+i,index=newWDset.index)
+        tempPD['E_PER'] = pd.Series(np.ones(newWDset.shape[0])*(year-1984)*12+1+i+1,index=newWDset.index)
+        tempPD['S_DAY'] = pd.Series(np.ones(newWDset.shape[0])*(year-1984)*365+sum(days_m[:i]),index=newWDset.index)
+        tempPD['E_DAY'] = pd.Series(np.ones(newWDset.shape[0])*(year-1984)*365+sum(days_m[:(i+1)]),index=newWDset.index)
+        tempPD['FLOW_M3-S'] = pd.Series(np.ones(newWDset.shape[0]),index=newWDset.index)
+        tempPD['START_YR'] = pd.Series(np.ones(newWDset.shape[0])*year,index=newWDset.index)
+        tempPD['END_YR'] = pd.Series(np.ones(newWDset.shape[0])*(year+1),index=newWDset.index)
+        tempPD['DATASET'] = pd.Series(['SACM']*newWDset.shape[0],index=newWDset.index)
+        stackedWDset = stackedWDset.append(tempPD)
+    
+    stackedWDset = stackedWDset.reindex(index=stackedWDset.index.astype(int))
+    stackedWDset = stackedWDset[stackedWDset.Flow_m3 < 0]
+        
+    pandasWD = pandasWD.append(stackedWDset)
+    
+    return pandasWD
