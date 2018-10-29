57d14f4154d623854f740ef0d3aef35a32f1298e
mrlmautner <37315823+mrlmautner@users.noreply.github.com>
2018-10-28 22:04:37 -0700

    Incorporate new geology thickness and municipality raster

--- ValleMexico_GD.py
+++ ValleMexico_GD.py
@@ -5,6 +5,8 @@ Created on Tue Jul 31 16:41:28 2018
 @author: MM
 """
 from gwscripts.dataprocessing import gengriddata as gen
+import numpy as np
+import pandas as pd
 
 ## Initialize model extent
 # Lower left corner: (455000, 2107000)
@@ -34,10 +36,17 @@ nrows = int((yur-yll)/cellsize) # nrows
 #    header = gen.getHeader(ncols,nrows,xll,yll,cellsize,-99999)
 #    dsAsArray = gen.sampleGD(filename,newfile,header,xll,yll,xur,yur,cellsize)
 
+##%%
+## Geology raster
+#filename = r'data_raw\GEOLOGY_ZONES_UNAM.asc'
+#newfile = r'data_output\GEO_VM.asc'
+#header = gen.getHeader(ncols,nrows,xll,yll,cellsize,-99999)
+#dsAsArray = gen.sampleGD(filename,newfile,header,xll,yll,xur,yur,cellsize)
+
 #%%
-# Geology raster
-filename = r'data_raw\GEOLOGY_ZONES_EXP-BASALT.asc'
-newfile = r'data_output\GEO_VM.asc'
+# Municipality raster
+filename = r'data_raw\MUN.asc'
+newfile = r'data_output\MUN_VM.asc'
 header = gen.getHeader(ncols,nrows,xll,yll,cellsize,-99999)
 dsAsArray = gen.sampleGD(filename,newfile,header,xll,yll,xur,yur,cellsize)
 
@@ -48,9 +57,29 @@ dsAsArray = gen.sampleGD(filename,newfile,header,xll,yll,xur,yur,cellsize)
 #header = gen.getHeader(ncols,nrows,xll,yll,cellsize,-99999)
 #dsAsArray = gen.averageGD(filename,newfile,header,xll,yll,xur,yur,cellsize)
 
+###%%
+## THICKNESS raster
+#filename = r'data_raw\THICK_LYR2.asc'
+#newfile = r'data_output\THICK2_VM.asc'
+#header = gen.getHeader(ncols,nrows,xll,yll,cellsize,-99999)
+#dsAsArray = gen.averageGD(filename,newfile,header,xll,yll,xur,yur,cellsize)
+#np.savetxt(newfile, dsAsArray, header=header, fmt="%1.2f",comments='')
+
 ##%%
 ## Active raster 
 #filename = r'data_raw\MODEL_ACTIVE.asc'
 #newfile = r'data_output\ACTIVE_VM.asc'
 #header = gen.getHeader(ncols,nrows,xll,yll,cellsize,-99999)
 #dsAsArray = gen.sampleGD(filename,newfile,header,xll,yll,xur,yur,cellsize)
+
+##%%
+## Well Data
+#years = np.arange(1984,2014)
+#dataset = years.copy()
+#dataset[0:3] = 1987
+#wellData = wellFeatures = pd.DataFrame(columns=['DATASET','X','Y','FLOW_M3-S','START_YR','END_YR','ID','S_PER','E_PER','S_DAY','E_DAY','Flow_m3','CVE_ENT','CVE_MUN','NOM_MUN','CODE'])
+#for y in range(0,int(len(dataset))):
+#    filename = r'data_raw\SACM_1987-2017\SACM_' + str(dataset[y]) + '.csv'
+#    wellData = gen.generateWellData(filename, wellData, years[y])
+#
+#wellData.to_csv('data_output\wells\PUMP_S.csv', columns=['DATASET','X','Y','FLOW_M3-S','START_YR','END_YR','ID','S_PER','E_PER','S_DAY','E_DAY','Flow_m3','CVE_ENT','CVE_MUN','NOM_MUN','CODE'])
