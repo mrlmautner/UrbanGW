2ac63db8fbadc68dc64d82d66dbebad894e6ae2b
mrlmautner <37315823+mrlmautner@users.noreply.github.com>
2019-12-20 15:05:57 -0800

    Update SA_main.sh

--- SA_main.sh
+++ SA_main.sh
@@ -3,7 +3,7 @@
 #SBATCH --distribution=plane=8  # Distribute in blocks of 8 to each node
 #SBATCH -p aqua                 # Partition option of AQUA is aqua
 #SBATCH -t 200:00:00            # Run time (hh:mm:ss) - 24 hours
-#SBATCH --mail-user=mmautner@ucdavis.edu              # address for email notification
+#SBATCH -M mmautner@ucdavis.edu              # address for email notification
 #SBATCH --mail-type=ALL         # email at Begin and End of job
 
 # Add my directory to the path to point to python.
