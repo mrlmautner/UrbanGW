83206499ac8ea9548c45477fe875b2e9a62d64b3
mrlmautner <37315823+mrlmautner@users.noreply.github.com>
2021-04-20 15:50:28 -0700

    Update SA_main.sh

--- SA_main.sh
+++ SA_main.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -l
-#SBATCH -n 168                  # Total number of processors to request
-#SBATCH --distribution=plane=8  # Distribute in blocks of 8 to each node
+#SBATCH -n 3                    # Total number of processors to request
 #SBATCH -p aqua                 # Partition option of AQUA is aqua
 #SBATCH -t 200:00:00            # Run time (hh:mm:ss) - 24 hours
-#SBATCH -M mmautner@ucdavis.edu              # address for email notification
+#SBATCH --mail-user=mmautner@ucdavis.edu              # address for email notification
 #SBATCH --mail-type=ALL         # email at Begin and End of job
 
 # Add my directory to the path to point to python.
@@ -12,4 +11,4 @@ export PATH=/zeolite/mmautner/miniconda3/bin:$PATH
 # To point to the correct MPI in my local folder
 module purge 
 
-mpirun --bind-to none python SA_main.py
+mpirun --bind-to none python SA_main_missing.py
