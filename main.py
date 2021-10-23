3aa82d023074e517c084032de939b5dc11320ab7
mrlmautner <37315823+mrlmautner@users.noreply.github.com>
2021-10-23 14:46:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ The model run will create an object with the following results:
 
     self.wwtps - list of randomly selected wastewater treatment plants where reuse has been implemented
     self.basins - list of the row and column where each infiltration basin has been implemented
-    self.mthlyleak - array with the total quantity of leaks in the system per month in m3 cost is the total number of interventions time their weights defined in the model set-up
-    self.cost - a summed relative cost of the alternative based on the interventions applied
+    self.mthlyleak - array with the total quantity of leaks in the system per month in m3
     self.wells - dictionary of well objects input into the MODFLOW model which includes positive flow from wastewater treatment plants, leaks, and recharge basins and negative flow from pumping wells
     self.landuse - dictionary that contains a raster and list of the percentage of land use type (NATURAL, URBAN, or WATER) per model cell for each model phase
 '''
